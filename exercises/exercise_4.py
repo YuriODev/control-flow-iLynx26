38a58a056ae11a7d022a3a82d6fc98ddb7319d07
iLynx26 <ivan.khaneiev@gmail.com>
2024-02-17 11:55:27 -0500

    exercise 4

--- exercises/exercise_4.py
+++ exercises/exercise_4.py
@@ -1,2 +1,14 @@
-# Your solution to Exercise 4
-
+def test_function(grade):
+    if 0 < grade < 4:
+        return("initial level")
+    elif 3 < grade < 7:
+        return("average level")
+    elif 6 < grade < 10:
+        return("sufficient level")
+    elif 9 < grade < 13:
+        return("high level")
+    else:
+        return("level is absent")
+    
+grade = int(input())
+print(test_function(grade))
