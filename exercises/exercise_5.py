bc9e636f18801b6ca240cd9158df90448dae4643
iLynx26 <ivan.khaneiev@gmail.com>
2024-02-17 12:15:27 -0500

    ex5

--- exercises/exercise_5.py
+++ exercises/exercise_5.py
@@ -1,2 +1,23 @@
-# Your solution to Exercise 5
+def test_function(a, b, c):
 
+    if a == b == 0:
+        return "No roots."
+    if a == 0:
+        x = -c / b
+        return str(x)
+    elif b == 0:
+        x = (-c / a) ** 0.5
+        return str(x)
+    else:
+        d = b ** 2 - 4 * a * c
+        if d < 0:
+            return "No roots."
+        x1 = (b + d) / 2 /a /c
+        x2 = (b - d) / 2 /a /c
+        return f"{x1} and {x2}"
+
+a = float(input())
+b = float(input())
+c = float(input())
+
+print(test_function(a, b, c))
