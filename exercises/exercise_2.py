f8478698777c5ee9818e2ffc328426fecca5d849
iLynx26 <ivan.khaneiev@gmail.com>
2024-02-17 11:46:38 -0500

    ex 2 + 3

--- exercises/exercise_2.py
+++ exercises/exercise_2.py
@@ -1,2 +1,12 @@
-# Your solution to Exercise 2
-
+def test_function(age):
+    if age <= 1:
+        return("You are an infant.")
+    elif age <= 13:
+        return("You are a child.")
+    elif age <= 20:
+        return ("You are a teenager.")
+    else:
+        return ("You are an adult.")
+    
+age = int(input())
+print(test_function(age))
--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -1,2 +1,16 @@
-# Your solution to Exercise 3
+def test_function(n):
+    if n == 0:
+        return("Green")
+    elif 0 < n < 11:
+        return "Red" if n % 2 else "Black"
+    elif 10 < n < 19:
+        return "Black" if n % 2 else "Red"
+    elif 18 < n < 29:
+        return "Red" if n % 2 else "Black"
+    elif 28 < n < 37:
+        return "Black" if n % 2 else "Red"
+    else:
+        return "The bet will not play!"
 
+n = int(input())
+print(test_function(n))
