c92df8a08a762b6dbf49cd7a04a24ed03891b527
iLynx26 <ivan.khaneiev@gmail.com>
2024-02-17 13:12:49 -0500

    ex9

--- exercises/exercise_9.py
+++ exercises/exercise_9.py
@@ -1,2 +1,13 @@
-# def test_function:
-#     re
+def test_function(n):
+    n3 = n%10
+    n2 = n%100 // 10
+    n1 = n//100
+    if n1 + n3 < n2:
+        return "<"
+    elif n1 + n3 > n2:
+        return ">"
+    else:
+        return "="
+    
+n = int(input())
+print(test_function(n))
