69462904bf1246131104634ffc40296e435b8b6a
iLynx26 <ivan.khaneiev@gmail.com>
2024-02-17 12:44:46 -0500

    ex7

--- exercises/exercise_7.py
+++ exercises/exercise_7.py
@@ -1,2 +1,27 @@
-# Your solution to Exercise 7
+def test_function(a, b, arithm):
+    if arithm == "+":
+        return a + b
+    elif arithm == "-":
+        return a - b
+    elif arithm == "/":
+        if b == 0:
+            return "Division by 0!"
+        return a / b
+    elif arithm == "*":	
+        return a * b
+    elif arithm == "pow":
+        return a ** b
+    elif arithm == "div":
+        if b == 0:
+            return "Division by 0!"
+        return a // b
+    elif arithm == "mod":
+        return a % b
+    else:
+        return "Something isn't right"
 
+a = float(input())
+b = float(input())
+arithm = str(input())
+
+print(test_function(a, b, arithm))
