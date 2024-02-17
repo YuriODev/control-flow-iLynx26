d25b746ca18c65aacb860aaf929c0cb8c7d750ea
iLynx26 <ivan.khaneiev@gmail.com>
2024-02-17 11:24:24 -0500

    ex 1

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,2 +1,13 @@
-# Your solution to Exercise 1
+def test_function(a, t):
+    if a == t:
+        return "Alex and Tatyana are of the same age."
+    elif a > t:
+        return "Alex is the eldest."
+    elif t > a:
+        return "Tatyana is the eldest."
+    
+a = int(input())
+t = int(input())
+
+print(test_function(a, t))
 
