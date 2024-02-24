8d28f9470409d12f12860d5c75fdbeba6c0bfd45
iLynx26 <ivan.khaneiev@gmail.com>
2024-02-24 12:02:29 -0500

    exercise 15

--- exercises/exercise_15.py
+++ exercises/exercise_15.py
@@ -1,20 +1,17 @@
 def test_function(day, month, year):
-    if day == 1:
-        if month == 1:
-            return(f"31.12.{year-1}")
-        elif month == 2 or month == 4 or month == 6 or month == 8 or month == 9 or month == 11:
-            return(f"31.{month-1}.{year}")
-        elif month == 3:
-            if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
-                return(f"29.{month-1}.{year}")
-            else:
-                return(f"28.{month-1}.{year}")
-        elif month == 5 or month == 7 or month == 10 or month == 11:
-            return(f"30.{month-1}.{year}")
+    if day == 31:
+        if month == 12:
+            return f"1.1.{year+1}"
         else:
-            return("Invalid month")
+            return f"1.{month+1}.{year}"
+    elif day == 30:
+        return f"1.{month+1}.{year}"
+    elif day == 29:
+        return f"1.{month+1}.{year}"
+    elif day == 28:
+        return f"1.{month+1}.{year}"
     else:
-        return(f"{day-1}.{month}.{year}")
+        return(f"{day+1}.{month}.{year}")
 
 day = int(input())
 month = int(input())
