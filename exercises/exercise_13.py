90d99590501964ce20544055f287ce87bc290b2a
iLynx26 <ivan.khaneiev@gmail.com>
2024-02-24 11:32:16 -0500

    13

--- exercises/exercise_13.py
+++ exercises/exercise_13.py
@@ -1,2 +1,11 @@
-# Your solution to Exercise 13
+def test_function(number):
+    digit_1 = number//1000
+    digit_2 = number%1000//100
+    digit_3 = number%100//10
+    digit_4 = number%10
+    if digit_1 != digit_2 != digit_3 != digit_4:
+        return True
+    return False
 
+number = int(input())
+print(test_function(number))
