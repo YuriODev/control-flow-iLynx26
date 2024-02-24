d2adaedc078225b0208ec1312a1bd306b984214f
iLynx26 <ivan.khaneiev@gmail.com>
2024-02-24 11:11:26 -0500

    ex 11

--- exercises/exercise_11.py
+++ exercises/exercise_11.py
@@ -1,2 +1,8 @@
-# Your solution to Exercise 11
+def test_function(year):
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+        return "Leap year."
+    else:
+        return "Ordinary year."
 
+year = int(input())
+print(test_function(year))
